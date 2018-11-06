4963a4fff9cc1a1d19adfb2bf9e8087b1342d4c8
Lukasz Szewczyk <szewczyk134@gmail.com>
2018-11-06 09:58:12 +0100

    Add first and last name to create customer component

--- saleor/static/dashboard-next/customers/components/CustomerCreateDetails/CustomerCreateDetails.tsx
+++ saleor/static/dashboard-next/customers/components/CustomerCreateDetails/CustomerCreateDetails.tsx
@@ -10,10 +10,14 @@ import i18n from "../../../i18n";
 export interface CustomerCreateDetailsProps {
   data: {
     email: string;
+    firstName: string;
+    lastName: string;
   };
   disabled: boolean;
   errors: Partial<{
     email: string;
+    firstName: string;
+    lastName: string;
   }>;
   onChange: (event: React.ChangeEvent<any>) => void;
 }
@@ -22,6 +26,7 @@ const decorate = withStyles(theme => ({
   root: {
     display: "grid" as "grid",
     gridColumnGap: theme.spacing.unit * 2 + "px",
+    gridRowGap: theme.spacing.unit * 3 + "px",
     gridTemplateColumns: "1fr 1fr"
   }
 }));
@@ -33,10 +38,32 @@ const CustomerCreateDetails = decorate<CustomerCreateDetailsProps>(
         <div className={classes.root}>
           <TextField
             disabled={disabled}
+            error={!!errors.firstName}
+            fullWidth
+            name="firstName"
+            label={i18n.t("First Name")}
+            helperText={errors.firstName}
+            type="firstName"
+            value={data.email}
+            onChange={onChange}
+          />
+          <TextField
+            disabled={disabled}
+            error={!!errors.lastName}
+            fullWidth
+            name="lastName"
+            label={i18n.t("Last Name")}
+            helperText={errors.lastName}
+            type="lastName"
+            value={data.lastName}
+            onChange={onChange}
+          />
+          <TextField
+            disabled={disabled}
             error={!!errors.email}
             fullWidth
             name="email"
-            label={i18n.t("E-mail")}
+            label={i18n.t("Email address")}
             helperText={errors.email}
             type="email"
             value={data.email}
